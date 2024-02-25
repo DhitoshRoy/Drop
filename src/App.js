9c8b209f516440eeb31ed35017483eec560f3da8
Dhitosh Roy <142252404+DhitoshRoy@users.noreply.github.com>
2024-02-25 22:11:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// imported dropdown from the component.
+import DropDown from "./Component/DropDown";
+
+function App() {
+  return (
+    <div className="App">
+      {/* imported dropdown component */}
+    <DropDown/>
+    </div>
+  );
+}
+
+export default App;
